0aed01d3d7008d90e7b597d40ff9c055e4c51433
Eric Crosson <esc@ericcrosson.com>
2015-03-03 11:13:45 -0600

    -mode suffix removed from minor mode

--- bury-successful-compilation.el
+++ bury-successful-compilation.el
@@ -87,7 +87,7 @@ Argument STRING provided by compilation hooks."
 	       'bury-successful-compilation))
 
 ;;;###autoload
-(define-minor-mode bury-successful-compilation-mode
+(define-minor-mode bury-successful-compilation
   "A minor mode to bury the *compilation* buffer upon successful
 compilations."
   :init-value nil
